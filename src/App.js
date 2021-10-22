8d31efb14d2cd3814a5b651981ba2d783af66ba7
Avinash-Ashokar <mail_avinashashokar@pm.me>
2021-10-22 13:59:20 +0530

    Made some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component.jsx";
 
+const HatsPage = () => {
+  return (
+    <div>
+      {" "}
+      <h1> Welcome to Hats Page </h1>{" "}
+    </div>
+  );
+};
+
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
