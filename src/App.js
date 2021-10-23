0acb8a519368c90b0071600c4523c6ccd5e45e85
Avinash-Ashokar <mail_avinashashokar@pm.me>
2021-10-23 22:46:05 +0530

    Implemented a sigin page and a shop page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
-import HomePage from "./pages/homepage/homepage.component.jsx";
-import ShopPage from "./pages/shop/shop.component.jsx";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import SigninAndSignup from "./pages/signin-and-signup/signin-and-signup.component";
+
+import Header from "./Components/header/header.component.jsx";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SigninAndSignup} />
       </Switch>
     </div>
   );
---
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./signin-and-signup.styles.scss";
+
+import Signin from "../../Components/signin/signin.component";
+
+const SigninAndSignup = () => (
+  <div className="signin">
+    {" "}
+    <Signin />
+  </div>
+);
+
+export default SigninAndSignup;
