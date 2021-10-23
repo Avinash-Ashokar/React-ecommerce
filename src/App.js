417ff5972f8b9e071f1ea0f136d56f6a39f6843a
Avinash-Ashokar <mail_avinashashokar@pm.me>
2021-10-23 19:00:27 +0530

    Made some changes

--- src/App.js
+++ src/App.js
@@ -4,22 +4,14 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component.jsx";
-
-const HatsPage = () => {
-  return (
-    <div>
-      {" "}
-      <h1> Welcome to Hats Page </h1>{" "}
-    </div>
-  );
-};
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/Components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./collection-preview.styles.scss";
+
+import CollectionItem from "../collection-item/collection-item.component.jsx";
+
+const CollectionPreview = ({ title, items }) => {
+  return (
+    <div className="collection-preview">
+      <h1 className="title"> {title.toUpperCase()} </h1>
+      <div className="preview">
+        {items
+          .filter((item, index) => index < 4)
+          .map(({ id, ...otherCollectionItemProps }) => (
+            <CollectionItem key={id} {...otherCollectionItemProps} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default CollectionPreview;
