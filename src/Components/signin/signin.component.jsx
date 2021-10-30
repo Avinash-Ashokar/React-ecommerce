a5ab5cd01e7ae8c44a9361ab593ce3b3ad3bf2e8
Avinash-Ashokar <mail_avinashashokar@pm.me>
2021-10-30 12:46:39 +0530

    Just commit

--- src/Components/signin/signin.component.jsx
+++ src/Components/signin/signin.component.jsx
@@ -32,6 +32,7 @@ class SignIn extends React.Component {
   render() {
     return (
       <div className="sign-in">
+      <h1> Hello World </h1>
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
